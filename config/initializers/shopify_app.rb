7c19582662a1bc8af92a4b4bdb4792629d94fe09
Kaiser Khan <kaiserwkhan@gmail.com>
2021-07-21 22:42:12 +1000

    Webhooks products/update is in working state

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "write_script_tags,read_products,write_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.reauth_on_access_scope_changes = true
   config.embedded_app = true
@@ -12,6 +12,7 @@ ShopifyApp.configure do |config|
   config.shop_session_repository = 'Shop'
   config.allow_jwt_authentication = true
   config.webhooks = [
+    {topic: 'products/update', address: "#{ENV['APP_URL']}/webhooks/product_update", format: 'json'},
     {topic: 'app/uninstalled', address: "#{ENV['APP_URL']}/webhooks/app_uninstalled", format: 'json'},
   ]
 end
