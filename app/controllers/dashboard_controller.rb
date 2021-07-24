340b44a76c2386d7b133a1fbab09d82a0dc3a821
Kaiser Khan <kaiserwkhan@gmail.com>
2021-07-24 20:14:16 +1000

    Dashboard asking for SearchesHelper

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,33 @@
+class DashboardController < AuthenticatedController
+  include SearchesHelper
+  
+  before_action :set_shop
+  before_action :check_billing
+
+  def index
+  	puts "DashboardController index start"
+    # Get all Products with unit set
+    current_products = Product.all()
+    currend_prod_ids = []
+    @shopify_products = []
+    #Generate array with products' shopify_id
+    current_products.each do |current|
+      currend_prod_ids.push(current.shopify_id)
+    end
+    #get all products from shopify
+    temp_shopify_products = ShopifyAPI::Product.find(:all)
+    # get the first 10 Products without unit set
+    temp_shopify_products.each do |current|
+      @shopify_products.push(current) if !currend_prod_ids.include?(current.id.to_s)
+      break if @shopify_products.count == 20
+    end
+    @product = Product.new
+  end
+
+  def show_collection
+  end
+
+  def manual_uninstall
+  end
+
+end
