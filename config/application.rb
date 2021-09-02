82556c2b495b5b32ec5f39973eea8dd92923b6fd
Kaiser Khan <kaiserwkhan@gmail.com>
2021-09-02 16:08:07 +1000

    Experiment: Remove nosniff

--- config/application.rb
+++ config/application.rb
@@ -22,21 +22,20 @@ module TurbolinksJwtTest2
     config.autoload_paths << Rails.root.join('lib') # adds Lib folder to autoloaded files
     config.autoload_paths += %W(#{config.root}/app/services)
 
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'none',
+      'X-XSS-Protection' => 'none',
+      'X-Download-Options' => 'noopen',
+      'X-Permitted-Cross-Domain-Policies' => 'none',
+      'Referrer-Policy' => 'none'
+    }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-
-    config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'none',
-      'X-XSS-Protection' => 'none',
-      'X-Content-Type-Options' => 'nosniff',
-      'X-Download-Options' => 'noopen',
-      'X-Permitted-Cross-Domain-Policies' => 'none',
-      'Referrer-Policy' => 'none'
-    }
 
     # config.middleware.insert_before 0, "Rack::Cors" do
     #   allow do
