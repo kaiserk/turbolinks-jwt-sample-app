45385a5015492253d1a2d2ca2fde3df9e050ae7b
Kaiser Khan <kaiserwkhan@gmail.com>
2021-09-02 16:38:01 +1000

    Revert mime-types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,7 +2,4 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register "text/richtext", :rtf
-Mime::Type.register "text/javascript", :js, %w( application/javascript application/x-javascript )
-Mime::Type.register "text/html", :html, %w( application/xhtml+xml ), %w( xhtml )
 
