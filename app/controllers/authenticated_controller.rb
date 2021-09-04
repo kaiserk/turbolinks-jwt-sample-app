aefc66de3d78c7556bbc98064ccb5c539e384c2e
Kaiser Khan <kaiserwkhan@gmail.com>
2021-09-04 21:25:05 +1000

    Add checks before check biliing

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class AuthenticatedController < ApplicationController
+  before_action :login_again_if_different_shop
+  around_filter :shopify_session
+  layout 'embedded_app'
   # include ShopifyApp::EnsureAuthenticatedLinks
   include ShopifyApp::Authenticated
 
@@ -35,4 +38,43 @@ class AuthenticatedController < ApplicationController
   #   # puts @shop_session.to_json
   #   @shop_session = @shop_session || ShopifyApp::SessionRepository.retrieve_shop_session(session[:shopify])
   # end
+
+  protected
+
+  def login_again_if_different_shop
+    puts "AuthenticatedC login_again_if_different_shop start"
+    #puts "session:" + session.to_json
+    if shop_session && params[:shop] && params[:shop].is_a?(String) && shop_session.url != params[:shop]
+      session[:shopify] = nil
+      session[:shopify_domain] = nil
+      #redirect_to_login
+      # In some cases redirect_to_login goes to /login without the shop parameter that's why I changed it
+      fullpage_redirect_to "#{main_app.root_url}/login?shop=#{params[:shop]}"
+    end
+    puts "AuthenticatedC login_again_if_different_shop end"
+  end
+
+  def shopify_session
+    if shop_session && shop_session.url && shop_session.token
+      puts "AuthenticatedC shopify_session is_shop_session"
+      begin
+        ShopifyAPI::Base.activate_session(shop_session)
+        yield
+      ensure
+        ShopifyAPI::Base.clear_session
+      end
+    else
+      puts "AuthenticatedC shopify_session is_not_shop_session"
+      #redirect_to_login
+      # In some cases redirect_to_login goes to /login without the shop parameter that's why I changed it
+      if params[:shop] && params[:shop].is_a?(String)
+        fullpage_redirect_to "#{main_app.root_url}/login?shop=#{params[:shop]}"
+      else
+        redirect_url = "#{main_app.root_url}/login"
+        redirect_url ["//login"]  = "/login"
+        redirect_to redirect_url
+      end
+    end
+  end
+
 end
