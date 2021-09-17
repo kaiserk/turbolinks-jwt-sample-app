8e40e26b71fd910d4aedfae6f1f2e36b2c0ef602
Kaiser Khan <kaiserwkhan@gmail.com>
2021-09-18 00:20:47 +1000

    ngrok new pattern fix

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.HOST = process.env.HOST || 'https://512afbc47b5b.ngrok.io'
+process.env.HOST = process.env.HOST || 'https://c492-2001-8003-230d-fb01-40ee-b6a3-1701-ed6e.ngrok.io'
 
 const environment = require('./environment')
 
