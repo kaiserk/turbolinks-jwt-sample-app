0ac33e46fb0dff08c7b6a504a892cd931884f4a3
Kaiser Khan <kaiserwkhan@gmail.com>
2021-10-18 22:58:10 +1100

    Revert cors changes

--- config/application.rb
+++ config/application.rb
@@ -16,26 +16,26 @@ module TurbolinksJwtTest2
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
-    config.action_dispatch.default_headers.delete('X-Frame-Options')
-
-    config.autoload_paths << Rails.root.join('lib') # adds Lib folder to autoloaded files
-    config.autoload_paths += %W(#{config.root}/app/services)
-
-    config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'none',
-      'X-XSS-Protection' => 'none',
-      'X-Download-Options' => 'noopen',
-      'X-Permitted-Cross-Domain-Policies' => 'none',
-      'Referrer-Policy' => 'none'
-    }
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
+    # config.action_dispatch.default_headers.delete('X-Frame-Options')
+    #
+    # config.autoload_paths << Rails.root.join('lib') # adds Lib folder to autoloaded files
+    # config.autoload_paths += %W(#{config.root}/app/services)
+    #
+    # config.action_dispatch.default_headers = {
+    #   'X-Frame-Options' => 'none',
+    #   'X-XSS-Protection' => 'none',
+    #   'X-Download-Options' => 'noopen',
+    #   'X-Permitted-Cross-Domain-Policies' => 'none',
+    #   'Referrer-Policy' => 'none'
+    # }
+    #
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
 
     # config.middleware.insert_before 0, "Rack::Cors" do
     #   allow do
